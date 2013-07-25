681ca1116989b3d05f90f8186743e1260509c1d8
Simon Kelly <skelly@dimagi.com>
2013-07-25 10:18:31 +0200

    bump version

--- ctable/tests/__init__.py
+++ ctable/tests/__init__.py
@@ -32,6 +32,5 @@ try:
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
     # otherwise debugging is a pain
-    import logging
     logging.exception(e)
     raise
