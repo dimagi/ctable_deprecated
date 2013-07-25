162735a7bb6a74271e2cbe9854bf80f07bd9bfde
Simon Kelly <skelly@dimagi.com>
2013-07-25 10:00:16 +0200

    fix unittest2 dep

--- setup.py
+++ setup.py
@@ -19,9 +19,10 @@ setup(
         'celery>=3.0.15',
         'psycopg2>=2.4.1',
         'pillowfluff>=0.0.1',
+        'unittest2>=0.5.1',
     ],
     tests_require=[
-        'unittest2>=0.5.1',
+
         'fakecouch>=0.0.3',
         'mock>=0.0.8'
     ]
