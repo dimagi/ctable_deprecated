af3d693f9e8bc53fe32bc80fadb765b4c0f651bf
Simon Kelly <skelly@dimagi.com>
2013-07-25 10:13:08 +0200

    log import errors

--- ctable/tests/__init__.py
+++ ctable/tests/__init__.py
@@ -22,10 +22,16 @@ class TestBase(TestCase):
     def tearDownClass(cls):
         cls.p1.stop()
 
-
-from ctable.tests.test_backends import *
-from ctable.tests.test_extract import *
-from ctable.tests.test_models import *
-from ctable.tests.test_signals import *
-from ctable.tests.test_util import *
-from ctable.tests.test_views import *
+try:
+    from ctable.tests.test_backends import *
+    from ctable.tests.test_extract import *
+    from ctable.tests.test_models import *
+    from ctable.tests.test_signals import *
+    from ctable.tests.test_util import *
+    from ctable.tests.test_views import *
+except ImportError, e:
+    # for some reason the test harness squashes these so log them here for clarity
+    # otherwise debugging is a pain
+    import logging
+    logging.exception(e)
+    raise
