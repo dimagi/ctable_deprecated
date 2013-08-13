aaa7d9ae6574de8aa02d9c78a581ddd49703bc2d
Simon Kelly <skelly@dimagi.com>
2013-08-13 12:45:25 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ctable',
-    version='0.0.5',
+    version='0.0.6',
     description='CouchDB view to SQL table',
     author='Dimagi',
     author_email='dev@dimagi.com',
