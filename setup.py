494f01f23c4f0f187af9df74ba9f864e86603164
Cory Zue <czue@dimagi.com>
2015-10-13 12:06:37 +0530

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ctable',
-    version='0.0.7',
+    version='0.0.8',
     description='CouchDB view to SQL table',
     author='Dimagi',
     author_email='dev@dimagi.com',
